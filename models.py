1957466aafd3c4338266b074f20edf455185bfe5
lsocpb <lukasssonik@gmail.com>
2023-11-19 21:49:42 +0100

    few changes

--- models.py
+++ models.py
@@ -6,13 +6,13 @@ from database import Base
 
 class UsersTransactions(Base):
     __tablename__ = 'user_transaction'
-    user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
-    transaction_id = Column(Integer, ForeignKey('transaction.transaction_id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    transaction_id = Column(Integer, ForeignKey('transaction.id'), primary_key=True)
 
 
 class User(Base):
     __tablename__ = 'user'
-    user_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     password_hash = Column(String(255), nullable=False)
     username = Column(String(255), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
@@ -21,7 +21,7 @@ class User(Base):
 
 class Company(Base):
     __tablename__ = 'company'
-    company_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     company_name = Column(String(255), unique=True, nullable=False)
     company_symbol = Column(String(10), unique=True, nullable=False)
 
@@ -31,9 +31,7 @@ class TransactionType(enum.Enum):
 
 class Transaction(Base):
     __tablename__ = 'transaction'
-    transaction_id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('user.user_id'))
-    company_id = Column(Integer, ForeignKey('company.company_id'))
+    id = Column(Integer, primary_key=True, index=True)
     amount = Column(Integer, nullable=False)
     price_per_unit = Column(DECIMAL(10, 2), nullable=False)
     transaction_date = Column(Date, nullable=False)
